373c77617ff5047da55826131b8b7689bb053ea7
naoto67 <uver.com67@gmail.com>
2020-09-10 13:39:22 +0900

    update

--- webapp/go/src/app/status.go
+++ webapp/go/src/app/status.go
@@ -11,18 +11,19 @@ func GoFuncGetStatus() {
 
 		for roomName, conns := range connMap {
 			logger.Infow("GoFuncGetStatus", "conns", conns)
+			if len(connMap) == 0 {
+				continue
+			}
 			go func() {
 				status, err := getStatus(roomName)
 				if err != nil {
 					logger.Infow("GoFuncGetStatus", "err", err)
-					return
 				}
 				for _, ws := range conns {
 					logger.Infow("GoFuncGetStatus", "ws", ws, "ws.Conn", ws.Conn)
 					err = ws.WriteJSON(status)
 					if err != nil {
 						logger.Info(err)
-						return
 					}
 				}
 			}()
